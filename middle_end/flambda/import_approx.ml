f657cb0b3da913021cab6e567f8587477c5fdac3
poechsel <pierre.oechsel@gmail.com>
2018-06-14 13:58:19 +0100

    [to-fold-1] rewrote max body size estimator

--- middle_end/flambda/import_approx.ml
+++ middle_end/flambda/import_approx.ml
@@ -100,7 +100,8 @@ let rec import_ex ex =
         ~invariant_params:(lazy invariant_params)
         ~specialised_args:Variable.Map.empty
         ~freshening:Freshening.Project_var.empty
-        ~direct_call_surrogates:Closure_id.Map.empty)
+        ~direct_call_surrogates:Closure_id.Map.empty
+        ~args:(Flambda.get_max_inlining_arguments ()))
   in
   let compilation_unit = Export_id.get_compilation_unit ex in
   match Compilenv.approx_for_global compilation_unit with
