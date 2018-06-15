8a22600617991d69fa9f89f57e7e3999502798b6
poechsel <pierre.oechsel@gmail.com>
2018-06-15 16:28:25 +0100

    shorten names in InliningArgs module

--- middle_end/inline_and_simplify.ml
+++ middle_end/inline_and_simplify.ml
@@ -740,7 +740,7 @@ and simplify_apply env r ~(apply : Flambda.apply) : Flambda.t * R.t =
     | None ->
       max_env_args
     | Some apply_args ->
-      InliningArgs.merge_inlining_arguments max_env_args apply_args
+      InliningArgs.merge max_env_args apply_args
   in
   let env = E.set_max_inlining_arguments env max_inlining_arguments in
   let max_inlining_arguments = Some max_inlining_arguments in
@@ -852,7 +852,7 @@ and simplify_full_application env r ~function_decls ~lhs_of_application
     match max_inlining_arguments with
     | None -> env
     | Some args ->
-      let merge_args = InliningArgs.merge_inlining_arguments (E.get_inlining_arguments env) args
+      let merge_args = InliningArgs.merge (E.get_inlining_arguments env) args
       in E.set_inlining_arguments env merge_args
   in
   Inlining_decision.for_call_site ~env ~r ~function_decls
