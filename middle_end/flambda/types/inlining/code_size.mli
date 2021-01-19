5bd53c37be8f914a6d26cfc297ce7e1a5be3426b
Pierre Oechsel <pierre.oechsel@gmail.com>
2021-01-19 14:05:05 +0000

    Move code size computation to its own file.

---
+++ middle_end/flambda/types/inlining/code_size.mli
@@ -0,0 +1,28 @@
+
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                       Pierre Chambart, OCamlPro                        *)
+(*           Mark Shinwell and Leo White, Jane Street Europe              *)
+(*                                                                        *)
+(*   Copyright 2013--2019 OCamlPro SAS                                    *)
+(*   Copyright 2014--2019 Jane Street Group LLC                           *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+[@@@ocaml.warning "+a-4-30-40-41-42"]
+
+open! Flambda.Import
+
+type t
+
+val expr_size : Simplify_envs.Downwards_env.t -> Expr.t -> t
+
+val of_int : int -> t
+val to_int : t -> int
+val smaller : t -> than:t -> bool
