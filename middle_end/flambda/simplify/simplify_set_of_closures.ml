0e554256a5d26af8dcc4b5d8651744c549618941
Pierre Oechsel <pierre.oechsel@gmail.com>
2021-04-02 16:35:50 +0100

    Provide a skeleton type for function decl when not rebuilding terms

--- middle_end/flambda/simplify/simplify_set_of_closures.ml
+++ middle_end/flambda/simplify/simplify_set_of_closures.ml
@@ -472,13 +472,14 @@ let simplify_function context ~used_closure_vars ~shareable_constants
     in
     let function_decl = FD.update_code_id function_decl new_code_id in
     let function_type =
-      (* When not rebuilding terms we cannot infer function types.  However
-         this shouldn't matter too much since inlining will typically be
-         disabled during such traversals. *)
+      (* When not rebuilding terms we only give a skeleton function type so
+         that we can distinguish between direct and indirect calls. *)
       if Are_rebuilding_terms.do_not_rebuild_terms
            (DA.are_rebuilding_terms dacc_after_body)
       then
-        Or_unknown_or_bottom.Unknown
+	T.create_non_inlinable_function_declaration
+	   ~code_id:new_code_id
+	   ~is_tupled:(FD.is_tupled function_decl)
       else
         (* We need to manually specify the cost metrics to use to ensure that
            they are the one of the body after simplification. *)
