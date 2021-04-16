dd443054db060800a150f95b3c188a2cdf3871bc
Guillaume Bury <guillaume.bury@gmail.com>
2021-04-16 15:29:17 +0200

    Update test

--- testsuite/tests/flambda-specialization/polymorphic_compare.ml
+++ testsuite/tests/flambda-specialization/polymorphic_compare.ml
@@ -27,10 +27,40 @@ let[@inline never] check_no_alloc line f x y expected_res =
 let[@inline] test_aux_lt x y =
   let res = if Obj.magic (x < y) < 2 then 1 else Sys.opaque_identity 42 in
   Some res
+let[@inline] test_aux_le x y =
+  let res = if Obj.magic (x <= y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
+let[@inline] test_aux_gt x y =
+  let res = if Obj.magic (x > y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
+let[@inline] test_aux_ge x y =
+  let res = if Obj.magic (x >= y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
+let[@inline] test_aux_eq x y =
+  let res = if Obj.magic (x = y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
+let[@inline] test_aux_ne x y =
+  let res = if Obj.magic (x != y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
+let[@inline] test_aux_cmp x y =
+  let res = if Obj.magic (compare x y) < 2 then 1 else Sys.opaque_identity 42 in
+  Some res
 
 let[@inline never] test_lt (x:float) y = test_aux_lt x y
+let[@inline never] test_le (x:int32) y = test_aux_le x y
+let[@inline never] test_gt (x:int64) y = test_aux_gt x y
+let[@inline never] test_ge (x:float) y = test_aux_ge x y
+let[@inline never] test_eq (x:float) y = test_aux_eq x y
+let[@inline never] test_ne (x:float) y = test_aux_ne x y
+let[@inline never] test_cmp (x:float) y = test_aux_cmp x y
 
 let () =
   check_no_alloc __LINE__ test_lt 5. 1. (Some 1);
+  check_no_alloc __LINE__ test_le 5l 1l (Some 1);
+  check_no_alloc __LINE__ test_gt 5L 1L (Some 1);
+  check_no_alloc __LINE__ test_ge 5. 1. (Some 1);
+  check_no_alloc __LINE__ test_eq 5. 1. (Some 1);
+  check_no_alloc __LINE__ test_ne 5. 1. (Some 1);
+  check_no_alloc __LINE__ test_cmp 5. 1. (Some 1);
   ()
 
