5dfddcefd7703560bf27665cddb2cc1c9fcfd9f2
Mark Shinwell <mshinwell@gmail.com>
2021-05-14 11:11:23 +0100

    PR#441 take two (#453)

--- middle_end/flambda/types/env/aliases.mli
+++ middle_end/flambda/types/env/aliases.mli
@@ -92,6 +92,8 @@ val get_aliases : t -> Simple.t -> Alias_set.t
 
 val get_canonical_ignoring_name_mode : t -> Name.t -> Simple.t
 
+(** Combine two alias trackers, assuming that any names they have in common
+    have the same name mode (if not, something is very wrong anyway). *)
 val merge : t -> t -> t
 
 val clean_for_export : t -> t
